745194438a1a27e914c32c21f6b4ad402114141f
darkhan <dildabekov95@gmail.com>
2024-04-21 14:42:27 +0500

    remove data loading from migrations whick creates tables

--- api/database/migrations/2024_04_04_231024_create_shops_table.php
+++ api/database/migrations/2024_04_04_231024_create_shops_table.php
@@ -13,14 +13,6 @@ return new class extends Migration {
             $table->softDeletes();
             $table->timestamps();
         });
-
-        $shops = \Illuminate\Support\Facades\DB::connection('oldDatabase')
-            ->table('shops')
-            ->get();
-
-        foreach ($shops as $shop) {
-            \App\Models\Shop::create(['id' => $shop->id, 'address' => $shop->address]);
-        }
     }
 
     public function down(): void
--- api/database/migrations/2024_04_07_033706_create_transaction_document_types_table.php
+++ api/database/migrations/2024_04_07_033706_create_transaction_document_types_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\TransactionDocumentType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,15 +12,6 @@ return new class extends Migration {
             $table->string('name');
             $table->timestamps();
         });
-
-        TransactionDocumentType::insert(
-            [
-                'name' => 'Акт приема передачи'
-            ],
-            [
-                'name' => 'Договор аренды'
-            ]
-        );
     }
 
     public function down(): void
