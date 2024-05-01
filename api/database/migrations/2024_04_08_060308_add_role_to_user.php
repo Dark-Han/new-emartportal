6559dcbb846d586c25925decd1ee8b67dd39de22
Darkhan Dildabekov <darkhan.dildabekov@sirenltd.com>
2024-05-01 20:46:34 +0500

    fix name of users table in migrations

--- api/database/migrations/2024_04_08_060308_add_role_to_user.php
+++ api/database/migrations/2024_04_08_060308_add_role_to_user.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->bigInteger('role')->nullable();
         });
     }
@@ -20,7 +20,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('role');
         });
     }
