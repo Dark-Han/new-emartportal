bceb276a421e29decdcebff52fc4b4b67eb90d3a
Darkhan Dildabekov <darkhan.dildabekov@sirenltd.com>
2024-05-04 16:55:06 +0500

    renamed from employee status

--- api/app/Models/UserStatus.php
+++ api/app/Models/UserStatus.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class EmployeeStatus extends Model
+class UserStatus extends Model
 {
     use SoftDeletes;
 
